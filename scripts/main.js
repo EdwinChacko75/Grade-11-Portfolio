5d187e40141f7504e67bb2f50bbf2956c3fd4653
EdwinChacko75 <78929985+EdwinChacko75@users.noreply.github.com>
2021-03-02 10:40:28 -0500

    lights

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,27 @@
-function welcome() {​​
-    let username = prompt("What is your name?");    
-    alert("Welcome to my website " + username)
-    document.getElementById("saywelcome").innerHTML = message; 
-}​​
+// function welcome() {​​
+//     let username = prompt("What is your name?");    
+//     alert("Welcome to my website " + username)
+//     document.getElementById("saywelcome").innerHTML = message; 
+// }​​
+
+function lights(){
+    let color = prompt("What color is the light now?");
+
+    if (color === "red") {
+        console.log("STOP!");
+    }
+    else if (color === "green")
+        console.log("GO!");
+
+    else if (color === "yellow") {
+        console.log("Stop if it is safe to do so");
+    }
+    else if (color === "amber") {
+        console.log("Stop if it is safe to do so")
+    }
+    else {
+        console.log("Invalid Input")
+    }
+}
+
+lights();
