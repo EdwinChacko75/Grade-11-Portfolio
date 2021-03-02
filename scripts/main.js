60e61bf655f58e54e7c7c1cb49dbb3d306a8c073
EdwinChacko75 <78929985+EdwinChacko75@users.noreply.github.com>
2021-03-02 10:01:30 -0500

    xc

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-
 function welcome() {​​
     let username = prompt("What is your name?");    
     alert("Welcome to my website " + username)
+    document.getElementById("saywelcome").innerHTML = message; 
 }​​
