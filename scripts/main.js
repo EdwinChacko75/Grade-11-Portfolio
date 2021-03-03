d09c7569ee9ddb0f9203d5e266ee14a70b91185d
EdwinChacko75 <78929985+EdwinChacko75@users.noreply.github.com>
2021-03-03 10:55:46 -0500

    Nav.1

--- scripts/main.js
+++ scripts/main.js
@@ -4,24 +4,24 @@
 //     document.getElementById("saywelcome").innerHTML = message; 
 // }​​
 
-function lights(){
-    let color = prompt("What color is the light now?");
+// function lights(){
+//     let color = prompt("What color is the light now?");
 
-    if (color === "red") {
-        alert("STOP!");
-    }
-    else if (color === "green")
-        alert("GO!");
+//     if (color === "red") {
+//         alert("STOP!");
+//     }
+//     else if (color === "green")
+//         alert("GO!");
 
-    else if (color === "yellow") {
-        alert("Stop if it is safe to do so");
-    }
-    else if (color === "amber") {
-        alert("Stop if it is safe to do so")
-    }
-    else {
-        alert("Invalid Input")
-    }
-}
+//     else if (color === "yellow") {
+//         alert("Stop if it is safe to do so");
+//     }
+//     else if (color === "amber") {
+//         alert("Stop if it is safe to do so")
+//     }
+//     else {
+//         alert("Invalid Input")
+//     }
+// }
 
-lights();
+// lights();
