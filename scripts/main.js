98d820bc2907d6d64e32063a4717043ed50da533
EdwinChacko75 <78929985+EdwinChacko75@users.noreply.github.com>
2021-03-10 20:10:08 -0500

    Updated Comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,3 @@
-// function welcome() {​​
-//     let username = prompt("What is your name?");    
-//     alert("Welcome to my website " + username)
-//     document.getElementById("saywelcome").innerHTML = message; 
-// }​​
-
-// function lights(){
-//     let color = prompt("What color is the light now?");
-
-//     if (color === "red") {
-//         alert("STOP!");
-//     }
-//     else if (color === "green")
-//         alert("GO!");
-
-//     else if (color === "yellow") {
-//         alert("Stop if it is safe to do so");
-//     }
-//     else if (color === "amber") {
-//         alert("Stop if it is safe to do so")
-//     }
-//     else {
-//         alert("Invalid Input")
-//     }
-// }
-
-// lights();
-
 // About Me Button To Top
 mybutton = document.getElementById("top-button");
 
@@ -41,3 +13,4 @@ function scrollFunction() {
 function topFunction() {
   document.documentElement.scrollTop = 0
 }
+//Projects Buttons Assignemnt
