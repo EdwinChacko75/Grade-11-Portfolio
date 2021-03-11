a3ea4b5a3ccab38cdb925ba5caaefdcd41a4a7b8
EdwinChacko75 <78929985+EdwinChacko75@users.noreply.github.com>
2021-03-10 22:47:55 -0500

    Assignment 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+//Vault Entry Message
+function vault() {
+  alert("Looks like you've stubmbled into my vault where I store my most classified projects...");
+}
 // About Me Button To Top
 mybutton = document.getElementById("top-button");
 
@@ -14,3 +18,43 @@ function topFunction() {
   document.documentElement.scrollTop = 0
 }
 //Projects Buttons Assignemnt
+  //Button 1
+function project1(){
+  alert("Project 1 Is Coming Soon");
+}
+
+  //Button 2
+function project2(){
+  alert("Project 2 Is Under Development");
+}
+
+  //Button 3
+function project3(){
+  alert("Debugging Project 3");
+}
+
+  //Button 4
+function project4(){
+  alert("Project 4 Is Almost Done");
+}
+
+ // Button 5
+function project5(){
+  alert("Stay Tuned For Project 5!");
+}
+  // Button 6
+function project6(){
+  alert("Touching Up Project 6");
+}
+  // Button 7
+function project7(){
+  alert("Project 7 Is Coming Along Great");
+}
+  // Button 8
+function project8(){
+  alert("Adding Semi-colons To Project 8");
+}
+  // Button 9
+function project9(){
+  alert("Project 9 Will Blow You Away");
+}
